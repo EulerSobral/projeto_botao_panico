0239ee9920b7f08cb7b07f6ea2001aa9ba87b1c0
Euler Pereira Sobral <eulerpereira60@gmail.com>
2025-07-07 15:04:19 -0300

    .

---
+++ src/componentes/Header.js
@@ -0,0 +1,16 @@
+import React from "react"; 
+
+
+function Header(){ 
+    return( 
+        <>
+        <header> 
+            <h1 className="title">Botão de pânico</h1>
+            <p className="text-title-1">Pressione qualquer botão para pedir ajudar</p> 
+            <p className="text-title-2">Escolha o serviço de emergência apropriado para a sua situação!</p> 
+        </header> 
+        </>
+    )
+}  
+
+export default Header
